3b31dbb38ab48ae4e3995606ee879d952bb569c4
DeMarcus Kirby <tkd024@gmail.com>
2020-06-09 10:41:20 -0600

    Add trails route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'weather#show'
       get '/backgrounds', to: 'background#show'
-      get '/foodie', to: 'foodie#show'
-      post '/users', to: 'users#create'
-      post '/sessions', to: 'sessions#create'
+      get '/trails', to: 'trails#index'
     end
   end
 end
