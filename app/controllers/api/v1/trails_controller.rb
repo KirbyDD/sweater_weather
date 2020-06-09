e7703960b23501bd982d848a5d1e9d30501ac83b
DeMarcus Kirby <tkd024@gmail.com>
2020-06-09 12:34:45 -0600

    Add serializer to controller

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::TrailsController < ApplicationController
   def index
     geocode = GeocodeService.new
     response = geocode.coordinates(params[:location])
-    trails = TrailsService.new.trails(response)
+    trails = TrailsService.new.trails(response, params[:location])  
+    render json: TrailsSerializer.new(TrailsFacade.new(trails))
   end
 end
