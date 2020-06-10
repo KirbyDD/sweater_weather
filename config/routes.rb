e6e5d6c4dbbbbe243de0e99d682f0f207bd624f9
DeMarcus Kirby <tkd024@gmail.com>
2020-06-10 07:09:42 -0600

    add session route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'weather#show'
       get '/backgrounds', to: 'background#show'
-      get '/foodie', to: 'foodie#show'
       post '/users', to: 'users#create'
-      post '/sessions', to: 'sessions#create'
+      post '/sessions', to: 'sessions#show'
     end
   end
 end
